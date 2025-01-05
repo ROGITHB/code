dc6f28eb9a4c704322ddd21282a8c5cffb9f0f57
ROGITHB <rb19042000@gmail.com>
2025-01-05 20:58:59 +0530

    Added init

--- p2.js
+++ p2.js
@@ -185,7 +185,46 @@ const n = userInput[0];
 const arr = userInput[1].split(" ").map(Number);
 mergeSort(arr, 0, arr.length - 1);
 console.log(arr.join(" "));
-
+//!Heaps sort
+function heapify(arr, n, i) {
+  let largest = i;
+  let l = 2 * i + 1; 
+  let r = 2 * i + 2; 
+  if (l < n && arr[l] > arr[largest]) {
+      largest = l;
+  }
+  if (r < n && arr[r] > arr[largest]) {
+      largest = r;
+  }
+  if (largest !== i) {
+      let temp = arr[i]; 
+      arr[i] = arr[largest];
+      arr[largest] = temp;
+      heapify(arr, n, largest);
+  }
+}
+function heapSort(arr) {
+  let n = arr.length;
+  for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+      heapify(arr, n, i);
+  }
+  for (let i = n - 1; i > 0; i--) {
+      let temp = arr[0];
+      arr[0] = arr[i];
+      arr[i] = temp;
+      heapify(arr, i, 0);
+  }
+}
+function printArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+      console.log(arr[i] + " ");
+  }
+  
+}
+const n = userInput[0];
+const arr = userInput[1].split(" ").map(Number);
+heapSort(arr);
+console.log(arr.join(" "));
 
  // diamond pattern
  let rows = Number(userInput[0]);
